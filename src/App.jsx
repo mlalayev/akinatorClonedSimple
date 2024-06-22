ea423cb3b0745dea8c7cf596dcd12804bfbaec26
Murad Lalayev <lalayevmurad@gmail.com>
2024-06-22 20:05:14 +0400

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,293 +23,13 @@ function App() {
     let updatedCharacters = [...filteredCharacters];
 
     switch (questionsData[currentQuestionIndex].id) {
-      case 1: // is your character real
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.real === "real");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.real === "not-real");
-        }
+      case 1: // Is your character real?
+        updatedCharacters = updatedCharacters.filter(character => character.real === (answer === "yes"));
         break;
 
-      case 2: // is your character male
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.gender === "male");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.gender === "female");
-        }
-        break;
+      case 2: // Is your character male?
+      updatedCharacters = updatedCharacters.filter(character => character.male === (answer === "yes"));
 
-      case 3: // human?
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.human === true);
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.human !== false);
-        }
-        break;
-
-      case 4: //fictional
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.fictional === true);
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.fictional === false);
-        }
-        break;
-
-      case 5: // female
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.gender === "female");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.gender === "male");
-        }
-        break;
-
-      case 6: //azerbaijani
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe === "Azerbaijan");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe !== "Azerbaijan");
-        }
-        break;
-
-      case 7: //turkish
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe === "Turkey");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe !== "Turkey");
-        }
-        break;
-
-      case 8: //adult
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.age === "adult");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.age !== "adult");
-        }
-        break;
-
-      case 9: //child
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.age === "child");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.age !== "child");
-        }
-        break;
-
-      case 10: // cartoon
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "cartoon");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "cartoon");
-        }
-        break;
-
-      case 11: // celebrity
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.alive === true);
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.alive !== true);
-        }
-        break;
-
-      case 12: //president
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "President");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "President");
-        }
-        break;
-
-      case 13: //superhero
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "superhero");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "superhero");
-        }
-        break;
-
-      case 14: //marvel universe
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe === "Marvel");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe !== "Marvel");
-        }
-        break;
-
-      case 15: // DC universe
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe === "DC");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.universe !== "DC");
-        }
-        break;
-
-      case 16: // entertainer
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "singer" || character.category === "actor" || character.category === "actress" || character.category === "dancer");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => !(character.category === "singer" || character.category === "actor" || character.category === "actress" || character.category === "dancer"));
-        }
-        break;
-
-      case 17: // nuclear safety inspector
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "Nuclear Safety Inspector");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "Nuclear Safety Inspector");
-        }
-        break;
-
-      case 18: // explorer
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "Explorer");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "Explorer");
-        }
-        break;
-
-      case 19: // monster
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "monster");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "monster");
-        }
-        break;
-
-      case 20: // singer-songwriter
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "singer" && character.occupation === "Singer-Songwriter");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => !(character.category === "singer" && character.occupation === "Singer-Songwriter"));
-        }
-        break;
-
-      case 21: // actor
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "actor" || character.category === "actress");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => !(character.category === "actor" || character.category === "actress"));
-        }
-        break;
-
-      case 22: // trickster
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "trickster");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "trickster");
-        }
-        break;
-
-      case 23: // Chancellor
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "Chancellor");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "Chancellor");
-        }
-        break;
-
-      case 24: // astronaut
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "astronaut");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "astronaut");
-        }
-        break;
-
-      case 25: // scientist
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "scientist");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "scientist");
-        }
-        break;
-
-      case 26: // poet
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "poet");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "poet");
-        }
-        break;
-
-      case 27: // dancer
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "dancer");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "dancer");
-        }
-        break;
-
-      case 28: // writer
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "writer");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "writer");
-        }
-        break;
-
-      case 29: // composer
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "composer");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "composer");
-        }
-        break;
-
-      case 30: // revolutionary
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "Revolutionary");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "Revolutionary");
-        }
-        break;
-
-      case 31: // dictator
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "Dictator");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "Dictator");
-        }
-        break;
-
-      case 32: // physiologist
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "Physiologist");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "Physiologist");
-        }
-        break;
-
-      case 33: // general secretary of the Communist Party
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation === "General Secretary of the Communist Party");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.occupation !== "General Secretary of the Communist Party");
-        }
-        break;
-
-      case 34: // ballet dancer
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "dancer" && character.occupation === "Ballet Dancer");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => !(character.category === "dancer" && character.occupation === "Ballet Dancer"));
-        }
-        break;
-
-      case 35: // animal
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "animal");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "animal");
-        }
-        break;
-
-      case 36: // leader
-        if (answer === "yes") {
-          updatedCharacters = updatedCharacters.filter(character => character.category === "leader");
-        } else if (answer === "no") {
-          updatedCharacters = updatedCharacters.filter(character => character.category !== "leader");
-        }
-        break;
 
 
       default:
@@ -335,10 +55,10 @@ function App() {
   useEffect(() => {
     if (filteredCharacters.length === 1) {
       setWinningText(translations.winner.text);
-      setWinningCharacter(filteredCharacters[0]); // Set winning character if only one remains
+      setWinningCharacter(filteredCharacters[0]);
     } else {
       setWinningText('');
-      setWinningCharacter(null); // Reset winning character if not exactly one
+      setWinningCharacter(null);
     }
   }, [filteredCharacters]);
 
@@ -363,8 +83,7 @@ function App() {
         <h2>{translations.uppertexth.second}</h2>
 
         <div className="questionHolder">
-          <p>{questionsData[currentQuestionIndex].id}</p>
-          <p>{questionsData[currentQuestionIndex][language]}</p>
+          <h3>{questionsData[currentQuestionIndex].id} : {questionsData[currentQuestionIndex][language]}</h3>
 
           {errorMessage && (
             <h1 className="error-message">{translations.error.message}</h1>
@@ -389,7 +108,7 @@ function App() {
         <div className="filteredCharacters">
           <h3>Filtered Characters:</h3>
           <ul style={{
-            marginTop: "500px"
+            marginTop: "5px"
           }}>
             {filteredCharacters.map((character, index) => (
               <li key={index}>{character.name}</li>
